70afad6b1cd7d97e52521fa5086c89236d749900
Tarek Aljundi <treetar321@gmail.com>
2026-05-07 13:08:29 +0300

    added retrain router

--- ml_platform/api/main.py
+++ ml_platform/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from ml_platform.api.routers import predict, registry
+from ml_platform.api.routers import predict, registry,retrain
 
 app = FastAPI(title="ML Platform")
 
 app.include_router(predict.router)
 app.include_router(registry.router)
+app.include_router(retrain.router)
 
 @app.get("/health")
 def health():
